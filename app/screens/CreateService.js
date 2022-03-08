7df841ed7c9cff6cff60d8a86e81a668f5fd7e8d
behrouz79 <behrouzamiri2014@gmail.com>
2022-03-08 17:02:00 +0330

    some

--- app/screens/CreateService.js
+++ app/screens/CreateService.js
@@ -1,9 +1,47 @@
 import React from "react";
+import ImagePicker from 'react-native-image-picker';
+import MainScreen from "../components/shared/MainScreen";
 
 
 const CreateService = () => {
-  return(
+  ImagePicker.showImagePicker(options, res => {
 
+    console.log('Response = ', res);
+
+    if (res.didCancel) {
+
+      console.log('User cancelled image picker');
+
+    } else if (res.error) {
+
+      console.log('ImagePicker Error: ', res.error);
+
+    } else if (res.customButton) {
+
+      console.log('User tapped custom button: ', res.customButton);
+
+      alert(res.customButton);
+
+    } else {
+
+      let source = res;
+
+      this.setState({
+
+        resourcePath: source,
+
+      });
+
+    }
+
+  });
+
+};
+
+  return(
+    <MainScreen>
+
+    </MainScreen>
   )
 }
 
