4b1b13c0551848b51a7532885edf8adaec5f7cda
behrouz <behrouzamiri2014@gmail.com>
2022-03-12 22:33:03 +0330

    some

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import {I18nManager} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import SplashScreen from './app/screens/SplashScreen';
@@ -6,6 +6,9 @@ import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './app/screens/LoginScreen';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import IndexRoutes from './app/routes/IndexRoutes';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import PushNotification from 'react-native-push-notification';
+import Firebase from '@react-native-firebase/app';
 
 I18nManager.allowRTL(true);
 I18nManager.forceRTL(true);
@@ -14,6 +17,22 @@ const Stack = createStackNavigator();
 const queryClient = new QueryClient();
 
 const App = () => {
+  PushNotification.configure({
+    onNotification: function (notification) {
+      notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true,
+    },
+    popInitialNotification: true,
+    requestPermissions: true,
+  });
+  useEffect(() => {
+    Firebase.initializeApp(App);
+  },[])
+
   return (
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
